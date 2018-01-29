fff74bcac0e89e74a1016d4836024e29717211cb
RANIA.LAGRICH <rania.lagrich@emines.um6p.ma>
2018-01-29 18:22:14 +0000

    mission 2 complete

--- app/src/main/java/com/authentification/khalidbaba/authentification/WelcomeActivity.java
+++ app/src/main/java/com/authentification/khalidbaba/authentification/WelcomeActivity.java
@@ -1,22 +1,29 @@
 package com.authentification.khalidbaba.authentification;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 /**
  * Created by khalidbaba on 28/01/2018.
  */
 
-public class WolcomeActivity extends Activity {
-
+public class WelcomeActivity extends Activity {
+TextView Email;
+String _email;
     //TODO : Declare all variables (TextView, String)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
+        Intent i =getIntent();
+        _email=i.getStringExtra("Email");
+        Email=(TextView) findViewById(R.id.email);
+        Email.setText(_email);
+;
         //TODO : Instantiate all objects
 
 
